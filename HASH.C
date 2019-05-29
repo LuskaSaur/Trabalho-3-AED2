13423f9e597439681e56a268afe69970dba33672
foolnando <47675174+foolnando@users.noreply.github.com>
2019-05-29 10:20:47 -0400

    Add files via upload

---
+++ HASH.C
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "HASH.h"
+
+hash* criaHash(int tableSize){
+  int i;
+  hash* h = (hash*) malloc(sizeof(hash));
+  if(h != NULL){
+    h->tableSize = tableSize;
+    h->item = (Produto*) malloc(sizeof(Produto));
+    if(h->item ==  NULL){
+      free(h);
+      return NULL;
+    }
+    h->qtd = 0;
+    for(i=0;i < (h->tableSize); i++){
+      h->item[i] = NULL;
+    }
+  }
+  return h;
+}
+
+void liberaHash(hash* h){
+  int i;
+  if(h != NULL){
+    for(i = 0; i < (h->tableSize);i++){
+      if(h->item[i] != NULL){
+        free(h->item[i]);
+      }
+    }
+    free(h->item);
+    free(h);
+  }
+}
+
+int chaveDivisao(int chave, int tableSize){
+  return(chave & 0x7FFFFFFF) % tableSize;
+}
+
+int chaveMultiplica(int chave, int tableSize){
+  float a = 0.618033898;
+  float val = chave*a;
+  val = val - (int)val;
+  return (int) (tableSize*val)
+}
+
+int insereHash_semColisao(hash* h, Produto *p){
+  if(h == NULL || h->qtd == h->tableSize){
+    return 0;
+  }
+  int chave = p.id;
+  int pos = chaveDivisao(chave, h->tableSize);
+  Produto *new;
+  new = (Produto*) malloc(sizeof(Produto));
+  if(new == NULL){
+    return 0;
+  }
+  new = p;
+  h->item->pos = novo
+  h->qtd++;
+  return 1;
+}
+
+int buscaHash_semColisao(hash* h, int id, Produto *p){
+  if(h == NULL){
+    return 0;
+  }
+  int pos = chaveDivisao(id, h->tableSize);
+  if(h->item->pos == NULL){
+    return 0;
+  }
+  *p = *(h->item->pos)
+  return 1;
+}
---
+++ HASH.h
@@ -0,0 +1,26 @@
+#ifndef HASH_H
+#define HASH_H
+
+typedef struct Produto{
+	int id; 		//4	(CHAVE)
+	int modelo;  	// 4
+	float custo; 	// 4
+	char nome[32];	// 32
+	int quantidade; //4
+}Produto;
+
+typedef struct hash{
+  int tableSize;
+  int qtd;
+  Produto **item;
+}hash;
+
+hash* criaHash(int tableSize); //cria tabela hash
+void liberaHash(hash* h); //esvazia tabela e destroi
+int chaveDivisao(int chave, int tableSize); //calculo da insercao do produto na tabela
+int chaveMultiplica(int chave, int tableSize); //outro metodo do calculo da insercao
+int insereHash_semColisa(hash* h, Produto p); //insere produto
+
+
+
+#endif
