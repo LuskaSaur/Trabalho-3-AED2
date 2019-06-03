a18634e4e26b94908ba9b2860f52fc537d10f9c5
foolnando <47675174+foolnando@users.noreply.github.com>
2019-06-03 16:57:04 -0400

    Add files via upload

---
+++ Trab3/Lib/HASH.h
@@ -0,0 +1,30 @@
+#ifndef HASH_H
+#define HASH_H
+
+typedef struct Produto{
+	int id; 		//4	(CHAVE)
+	int modelo;  	// 4
+	float custo; 	// 4
+	char nome[32];	// 32
+	int quantidade; //4
+}Produto;
+
+typedef struct hash{
+  int tableSize;
+  int qtd;
+  Produto **item;
+}hash;
+
+hash* criaHash(int tableSize); //cria tabela hash
+void liberaHash(hash* h); //esvazia tabela e destroi
+int chaveDivisao(int chave, int tableSize); //calculo da insercao do produto na tabela
+int chaveMultiplica(int chave, int tableSize); //outro metodo do calculo da insercao
+int sondagemLinear(int pos,int i, int tableSize); //sondagem linear para colisa
+int insereHash_enderAberto(hash* h, Produto *p); //insere produto colisao
+int buscaHash_enderAberto(hash* h, int id, Produto *p); //busca produto colisao
+
+
+
+
+
+#endif
